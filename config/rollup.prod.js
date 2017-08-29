dbcbab1e46c54f880d702f63734f857df6003370
Yukino Song <syqlds@126.com>
2017-08-30 00:57:02 +0800

    Modify build script

--- config/rollup.prod.js
+++ config/rollup.prod.js
@@ -1,9 +1,5 @@
-import chalk from 'chalk'
 // Import base config
 import base from './rollup.base'
-
-// Log build environment
-console.log('Build Target:', chalk.bold.green(process.env.BUILD_TARGET || 'development'))
 
 if (process.env.BUILD_TARGET === 'production') base.output.file = `${base.proPath}/${base.bundle}.min.js`
 else base.output.file = `${base.proPath}/${base.bundle}.dev.js`
