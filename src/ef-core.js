15af25c320f76bb14b6d5f422872405ab774a6cd
Yukino Song <yukino@yuki.network>
2021-10-05 05:16:09 +0800

    Bump version

--- src/ef-core.js
+++ src/ef-core.js
@@ -99,7 +99,7 @@ const create = (ast) => {
 	return EFComponent
 }
 
-let coreVersion = '0.14.1'
+let coreVersion = '0.14.2'
 
 if (process.env.NODE_ENV !== 'production') {
 	coreVersion = `${coreVersion}+debug`
